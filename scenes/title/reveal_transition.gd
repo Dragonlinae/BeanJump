8f320b224a08d6549e2cbc31720202d9f28cfff7
Dragonlinae <joshuahsu108@gmail.com>
2024-09-17 09:54:56 -0700

    Add title screen

---
+++ scenes/title/reveal_transition.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+@export var animation: AnimationPlayer = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation.play("Intro")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func delete():
+	queue_free()
+	pass
